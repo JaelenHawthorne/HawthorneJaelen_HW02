c944830fb7a301bab30d6d9837f62c3e5e5b75f6
Jaelen Hawthorne <jeh160230@utdallas.edu>
2020-10-07 13:41:38 -0400

    cube and sphere
    created color changing cube and size changing sphere from brackeys video

---
+++ Assets/Color/Editor/CubeEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(cube))]
+public class CubeEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        cube Cube = (cube)target;
+
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Generate Color"))
+        {
+            Cube.GenerateColor();
+        }
+
+        if (GUILayout.Button("Reset"))
+        {
+            Cube.Reset();
+        }
+
+        GUILayout.EndHorizontal();
+    }
+
+
+
+}
